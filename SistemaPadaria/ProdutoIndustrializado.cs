574c8f2b9a55060a833301a1c010362099f16e27
00-italo <italoadr75@gmail.com>
2026-05-18 02:50:04 -0300

    feat: CRUD de produtos implementado

---
+++ SistemaPadaria/ProdutoIndustrializado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPadaria
+{
+    internal class ProdutoIndustrializado : Produto
+    {
+        public string Marca {  get; set; }
+        public string CodigoBarras { get; set; }
+    }
+}
---
+++ SistemaPadaria/Program.cs
@@ -0,0 +1,3 @@
+﻿using SistemaPadaria;
+
+ProdutoRepositorio ProdutoRep = new ProdutoRepositorio();
